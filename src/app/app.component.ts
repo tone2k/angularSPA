cfb511297047e27074fb5df9c177baa938338d39
Tony Thompson <penguinprofession@icloud.com>
2019-04-09 13:18:38 -0400

    leveraged property and event binding to add new post to list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mean-app';
+  storedPost = [];
+
+  onPostAdded(post) {
+    this.storedPost.push(post);
+  }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -16,5 +16,5 @@ export class PostListComponent {
   //   { title: 'Third Post', content: 'This is the third post' }
   // ];
 
-  posts = [];
+  @Input() posts = [];
 }
